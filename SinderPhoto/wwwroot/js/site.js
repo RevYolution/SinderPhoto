5612848b11b5690b8e8da7636a92c7c9b91c8745
Jon Rice <revyolution1120@gmail.com>
2020-04-16 19:30:33 -0700

    First look at app TODO's added in

--- SinderPhoto/wwwroot/js/site.js
+++ SinderPhoto/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    var portfolioDrop = $("#portfolio-base");
+    var galleryImages = $(".gallery").find("img");
 
-// Write your JavaScript code.
+    //TODO: Set highlight images here//
+    var images = [
+        "../sample_pics/sample1.jpg",
+        "../sample_pics/sample2.jpg",
+        "../sample_pics/sample3.jpg",
+        "../sample_pics/sample4.jpg"
+    ]
+    //portfolioDrop.click(function () {
+    //    $(this).text("Changed with jQuery");
+    //});
+
+    portfolioDrop.on({
+        click: function () {
+            //$(this).append("<li> First album </li>")
+            $(this).text("Can be dropdown of photo albums")
+        },
+        mouseenter: function () {
+            $(this).css("cursor", "pointer");
+        }
+    });
+
+    var i = 0;
+    setInterval(function () {
+        i = (i + 1) % images.length;
+        galleryImages.fadeOut(function () {
+            $(this).attr("src", images[i]);
+            $(this).fadeIn(1500);
+        });
+    }, 4000);
+
+
+});
+
